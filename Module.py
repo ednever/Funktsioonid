e5ea42894090ad034e8a057c61efe75d82e57b48
opilane <opilane@HP-CZC6297BBL>
2021-12-15 10:56:44 +0200

    New

--- Module.py
+++ Module.py
@@ -1,4 +1,5 @@
-def Kolmnurga_pindala(külg:float,kõrgus:float,):
+#funktsioonid
+def Kolmnurga_pindala(külg:float,kõrgus:float):
     """Leiab kolmnurga pindalat. On antud kõrgus ja külje pikkus. Tagastab S float formaadis
     :param float külg: Kolmnurga külje pikkus
     :param float kõrgus: Kõrgus vastav küljele
@@ -10,3 +11,32 @@ def Kolmnurga_pindala(külg:float,kõrgus:float,):
         S=0.5*külg*kõrgus
 
     return S
+
+#funktsioonide_loomine 1
+def arithmetic(a:float,b:float,tehe:str):
+    """Liitmine, lahutamine, korrutamine ja jagamine. On antud a, b ja tehe. Tagastab vastus float formaadis
+    :param float a: Esimene arv
+    :param float a: Teine arv
+    :param str c: Aritmeetilise tehe
+    :rtype: var
+    """
+    if c in ["+","-","*","/"]:
+        if c=="/" and b==0:
+            vastus="Div/0"
+        else:
+            vastus=eval(str(a)+c+str(b))
+    else:
+        vastus="Tundmatu tehe!"
+    return vastus
+
+#funktsioonide_loomine 2
+def is_year_leap(aasta:int)->bool:
+    """Sisetame aasta number ja tagastame True, kui aasta on liigaasta ja False, kui ei ole
+    :param int aasta: Aasta järjekordne number
+    :rtype: bool
+    """
+    if aasta%4==0:
+        tulemus=True
+    else:
+        tulemus=False
+    return tulemus
---
+++ funktsioonide_loomine.py
@@ -0,0 +1,10 @@
+from Module import *
+#1
+math=arithmetic(2,3,"-")
+print(math)
+
+#2
+a=is_year_leap(int(input("Siseta aasta number - ")))
+print(a)
+
+#3
